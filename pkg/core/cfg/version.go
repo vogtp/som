0828008148c5d5efab606ff2c03fcdf357b73855
Patrick Vogt <patrick.vogt@unibas.ch>
2022-08-18 14:33:40 +0200

    push version

--- pkg/core/cfg/version.go
+++ pkg/core/cfg/version.go
@@ -6,9 +6,9 @@ const (
 	// VersionMajor major version
 	VersionMajor = 0
 	// VersionMinor minor version
-	VersionMinor = 5
+	VersionMinor = 6
 	// VersionPatch patch level
-	VersionPatch = 3
+	VersionPatch = 0
 )
 
 var (
