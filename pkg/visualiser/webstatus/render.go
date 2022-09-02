26f4b40a05f266ec5f3f3a489c5d56cb84cd7c78
Patrick Vogt <patrick.vogt@unibas.ch>
2022-09-02 15:22:54 +0200

    render json

--- pkg/visualiser/webstatus/render.go
+++ pkg/visualiser/webstatus/render.go
@@ -1,13 +1,47 @@
 package webstatus
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func (s *WebStatus) render(w http.ResponseWriter, r *http.Request, templateName string, data any) {
+	ah := r.Header.Get("Accept")
+	s.hcl.Debugf("Accept header: %s", ah)
 
-	err := templates.ExecuteTemplate(w, templateName, data)
-	if err != nil {
-		s.hcl.Errorf("cannot render template %s: %v", templateName, err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if strings.Contains(ah, "html") {
+		if err := templates.ExecuteTemplate(w, templateName, data); err != nil {
+			s.hcl.Errorf("cannot render template %s: %v", templateName, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if strings.Contains(ah, "application/json") {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			s.hcl.Errorf("cannot encode data of %s to json: %v", templateName, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		return
 	}
+
+	// XML encoder is less flexible than the json encoder -> does not work
+	// if strings.Contains(ah, "application/xml") {
+	// 	w.Header().Set("Content-Type", "application/xml; charset=utf-8")
+	// 	if err := xml.NewEncoder(w).Encode(&data); err != nil {
+	// 		s.hcl.Errorf("cannot encode data of %s to xml: %v", templateName, err)
+	// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 		return
+	// 	}
+	// 	return
+	// }
+
+	err := fmt.Errorf("unsupported content-type: %v", ah)
+	s.hcl.Warnf("Cannot render %s: %v", templateName, err)
+	http.Error(w, err.Error(), http.StatusBadRequest)
+
 }
