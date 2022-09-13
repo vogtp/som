14cc577a6264d186dfc5dd694917db129ceb6a12
Patrick Vogt <patrick.vogt@unibas.ch>
2022-09-13 12:48:51 +0200

    get fileMsgItem from model

---
+++ pkg/visualiser/webstatus/database/ent/file_custom.go
@@ -0,0 +1,20 @@
+package ent
+
+import (
+	"github.com/vogtp/som/pkg/core/mime"
+	"github.com/vogtp/som/pkg/core/msg"
+)
+
+// MsgItem converts to msg.FileMsgItem
+func (f File) MsgItem() msg.FileMsgItem {
+	return msg.FileMsgItem{
+		ID:   f.UUID,
+		Name: f.Name,
+		Type: mime.Type{
+			MimeType: f.Type,
+			Ext:      f.Ext,
+		},
+		Size:    f.Size,
+		Payload: f.Payload,
+	}
+}
