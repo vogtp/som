2ccdda44b5ad4168ae847d937ea7e06fd243f449
Patrick Vogt <patrick.vogt@unibas.ch>
2022-12-15 11:35:48 +0100

    reenable pw clean

--- pkg/stater/user/storage.go
+++ pkg/stater/user/storage.go
@@ -58,9 +58,8 @@ func (us *store) save() error {
 }
 
 func (us *store) cleanupPasswords() {
-	// FIXME PW cleanup disabled for now
-	// for n, u := range us.data {
-	// 	u.deleteOldPasswords()
-	// 	us.data[n] = u
-	// }
+	for n, u := range us.data {
+		u.deleteOldPasswords()
+		us.data[n] = u
+	}
 }
