613663aeba3bce817788f805bbfbe8eefa77cfea
Patrick Vogt <patrick.vogt@unibas.ch>
2026-01-20 17:29:35 +0100

    default password

--- pkg/core/cfg/amqp.go
+++ pkg/core/cfg/amqp.go
@@ -14,11 +14,13 @@ const (
 	AmqpPasswort = "amqp.password"
 )
 
+var amqpDefaultPassword = "" // set this in a init func in a ignored file in order for the tests to work
+
 func amqpFlags() {
 	pflag.String(AmqpHost, "localhost", "Host running the AMQP server (e.g. rabbitmq)")
 	pflag.Int(AmqpPort, 5672, "Port the AMQP server is running on")
 	pflag.String(AmqpUser, "som", "AMQP user")
-	pflag.String(AmqpPasswort, "", "AMQP user password")
+	pflag.String(AmqpPasswort, amqpDefaultPassword, "AMQP user password")
 }
 
 func AmqpURL() string {
