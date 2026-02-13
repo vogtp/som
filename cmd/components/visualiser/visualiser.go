4743babc83cd578a9e1119deca330068685a506b
Patrick Vogt <patrick.vogt@unibas.ch>
2026-02-13 10:42:27 +0100

    fix

--- cmd/components/visualiser/visualiser.go
+++ cmd/components/visualiser/visualiser.go
@@ -5,9 +5,7 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/spf13/pflag"
 	"github.com/vogtp/som/pkg/core"
-	"github.com/vogtp/som/pkg/core/cfg"
 	"github.com/vogtp/som/pkg/visualiser"
 	"github.com/vogtp/som/szenarios"
 )
@@ -17,7 +15,7 @@ func main() {
 	defer stop()
 	// szenarios.Load() has to be replace by ones own szenario config
 	szCfg := szenarios.Load()
-	pflag.String(cfg.DataDir, "data", "Folder to save output like screenshots in")
+	// pflag.String(cfg.DataDir, "data", "Folder to save output like screenshots in")
 	close, err := visualiser.Run("som.visualiser", core.Szenario(szCfg))
 	defer close()
 	if err != nil {
