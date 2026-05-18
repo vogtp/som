c3173ffdc853da3e33b9351c8971c2a5a0b6fd95
Patrick Vogt <patrick.vogt@unibas.ch>
2026-05-18 12:43:13 +0200

    fix adfs login not to annoy soc

--- szenarios/adfs_login.go
+++ szenarios/adfs_login.go
@@ -17,21 +17,21 @@ func AdfsLogin(engine szenario.Engine, s szenario.Szenario) (err error) {
 		chromedp.SendKeys(pwInputID, s.User().Password()+"\r", chromedp.ByID),
 	)
 
-	// trustBuID := `#idSIButton9`
-	// trust := "trust"
-	// notrust := "notrust"
-	// opt := engine.Either("Is trust displayed",
-	// 	szenario.EitherOption{ID: trust, Action: chromedp.WaitVisible(trustBuID, chromedp.ByID)},
-	// 	szenario.EitherOption{ID: notrust, Action: chromedp.WaitNotVisible(trustBuID, chromedp.ByID)},
-	// )
+	trustBuID := `#idSIButton9`
+	trust := "trust"
+	notrust := "notrust"
+	opt := engine.Either("Is trust displayed",
+		szenario.EitherOption{ID: trust, Action: chromedp.WaitVisible(trustBuID, chromedp.ByID)},
+		szenario.EitherOption{ID: notrust, Action: chromedp.WaitNotVisible(trustBuID, chromedp.ByID)},
+	)
 
-	// sel := <-opt
-	// if sel == trust {
-	// 	engine.Step("Trust ourself",
-	// 		chromedp.Click(trustBuID, chromedp.ByID),
-	// 		chromedp.WaitNotVisible(trustBuID, chromedp.ByID),
-	// 	)
-	// }
+	sel := <-opt
+	if sel == trust {
+		engine.Step("Trust ourself",
+			chromedp.Click(trustBuID, chromedp.ByID),
+			// chromedp.WaitNotVisible(trustBuID, chromedp.ByID),
+		)
+	}
 
 	return nil
 }
